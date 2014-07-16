a3918beee036a18c29bd408cdcf1fb4542c0c006
markrabins <markrabins@gmail.com>
2014-07-16 16:21:34 -0700

    Initial Commit

---
+++ Race to the Top/MRViewController.h
@@ -0,0 +1,13 @@
+//
+//  MRViewController.h
+//  Race to the Top
+//
+//  Created by Mark Rabins on 7/16/14.
+//  Copyright (c) 2014 edu.self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRViewController : UIViewController
+
+@end
---
+++ Race to the Top/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Race to the Top
+//
+//  Created by Mark Rabins on 7/16/14.
+//  Copyright (c) 2014 edu.self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MRAppDelegate class]));
+    }
+}
